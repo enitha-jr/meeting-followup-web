f8edc4ebb149563b48acced83c64cabfe85799af
enitha-jr <enitharajeshkumar@gmail.com>
2024-11-28 20:54:24 +0530

    meet-task

--- src/pages/MeetTasks.jsx
+++ src/pages/MeetTasks.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import { FiPlus } from "react-icons/fi";
 import './styles/MeetTasks.css'
+import { useState } from 'react';
 
 const MeetTasks = () => {
+  const [showForm, setShowForm] = useState(false);
+  const showTaskform = () => {
+    setShowForm(!showForm)
+
+    console.log("show task form")
+  }
   return (
     <div className='meettask-content'>
-      <div className='add-button' onclick ><FiPlus/>ADD</div>
+      <div className='add-button' onClick={showTaskform} ><FiPlus/>ADD</div>
+      {
+        showForm && 
+        <div className='task-container'>
+          <div>
+            <h5>ASSIGN TASK</h5>
+          </div>
+          <form>
+            <h1>hii</h1>
+          </form>
+        </div>
+      }
     </div>
   )
 }
